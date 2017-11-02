83812537377577cc523958ca0644bb4e1b6bb05f
Keidler Michael <keidlerm@grob.local>
2017-11-02 15:22:01 +0100

    fb integation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,15 +9,19 @@ import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private fb: Facebook) {
+  private fb = new Facebook();
 
+  constructor(public navCtrl: NavController) {
+    
   }
 
     FBLogin(){
+      
         console.log('crap');
 
         this.fb.login(['public_profile', 'user_friends', 'email'])
         .then((res: FacebookLoginResponse) => console.log('Logged into Facebook!', res))
         .catch(e => console.log('Error logging into Facebook', e));
+ /*       */
     }  
 }
