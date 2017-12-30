9c23d948148c7f1553864feadeb8cc8a14a92a59
mahik <mahik@Tinas-Air.fritz.box>
2017-12-30 22:56:42 +0100

    minor fixes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -108,14 +108,14 @@ export class HomePage {
           }).then(function(result) {
             // This gives you a Google Access Token.
             // You can use it to access the Google API.
-            var token = result.credential.accessToken;
+            // var token = result.credential.accessToken;
             // The signed-in user info.
-            var user = result.user;
+            // var user = result.user;
             // ...
           }).catch(function(error) {
             // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
+            // var errorCode = error.code;
+            // var errorMessage = error.message;
           });
         }
         else {
