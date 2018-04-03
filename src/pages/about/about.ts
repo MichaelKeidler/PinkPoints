bc97ed8a77861776169bb5c97a3720da50ff8d07
Keidler Michael <keidlerm@grob.local>
2018-04-03 14:30:43 +0200

    new version

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -21,7 +21,18 @@ export class AboutPage {
                            {value:20, label:'20 PinkyPoints'},
                            {value:40, label: '40 PinkyPoints'},
                            {value:100, label:'100 PinkyPoints'},
-                           {value:500, label: '500 PinkyPoints'}]
+                           {value:500, label: '500 PinkyPoints'},                           
+                           {value:-0.5, label: '-0.5 BlackPoints'},
+                           {value:-1, label:'-1 BlackPoints'},
+                           {value:-2, label: '-2 BlackPoints'},
+                           {value:-3, label:'-3 BlackPoints'},
+                           {value:-5, label: '-5 BlackPoints'},
+                           {value:-8, label:'-8 BlackPoints'},
+                           {value:-13, label: '-13 BlackPoints'},
+                           {value:-20, label:'-20 BlackPoints'},
+                           {value:-40, label: '-40 BlackPoints'},
+                           {value:-100, label:'-100 BlackPoints'},
+                           {value:-500, label: '-500 BlackPoints'}]
 
   constructor(public navCtrl: NavController) {
 
